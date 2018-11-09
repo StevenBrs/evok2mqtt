41836cbb33bcd44db4d2f772de5c22993e6462b6
StevenBrs <steven.broos@gmail.com>
2018-11-09 20:37:26 +0100

    Added watchdog

--- evok2mqtt.py
+++ evok2mqtt.py
@@ -1,17 +1,21 @@
 import json
 import websocket
 import paho.mqtt.client as mqtt
+import time
+from subprocess import call
 
-#try:
-#  import thread
-#except ImportError:
-#  import _thread as thread
-#import time
+try:
+  import thread
+except ImportError:
+  import _thread as thread
+
 
 def ws_on_message(ws, message):
   payloads = json.loads(message)
   for payload in payloads:
-    if payload['dev'] not in ['wd', 'ai']:
+    if payload['dev'] == 'wd':
+      wds[payload['circuit']] = time.time()
+    elif payload['dev'] not in ['wd', 'ai']:
       print "WS IN  : ", payload
       topic = "neuron/" + payload["dev"] + "/" + payload["circuit"]
       print "MC OUT : ", topic, json.dumps(payload)
@@ -46,7 +50,7 @@ def ws_on_close(ws):
   print("### websocket to evok closed ###")
 
 def mc_on_connect(mc, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
+  #print("Connected with result code "+str(rc))
   mc.subscribe("neuron/#")
   
 
@@ -61,10 +65,30 @@ def ws_on_open(ws):
     print("thread terminating...")
   thread.start_new_thread(run, ())
 
+def watchdog():
+  while True:
+    ok = True
+    for wd in wds:
+      if time.time() - wds[wd] > 5:
+        ok = False
+        print "Neurontcp reset : ", wd, " (", wds[wd], ")"
+        call (["/bin/systemctl", "restart", "neurontcp"])
+        time.sleep(5)
+
+    if ok:
+      text = ""
+      for wd in wds:
+        text += " - " + wd + " : "
+        text += "%.2f " % (time.time() - wds[wd])
+#      print "Watchdogs OK", text
+
+    time.sleep(5)
+
 
 if __name__ == "__main__":
+  wds = {}
 
-  websocket.enableTrace(False)
+  #websocket.enableTrace(False)
   ws = websocket.WebSocketApp("ws://127.0.0.1/ws",
                 on_message = ws_on_message,
                 on_error = ws_on_error,
@@ -77,11 +101,10 @@ if __name__ == "__main__":
   mc.connect("127.0.0.1", 1883, 60)
 
   try:
+    thread.start_new_thread(watchdog,())
     mc.loop_start()
+    #thread.start_new_thread(ws.run_forever,())
     ws.run_forever()
   except KeyboardInterrupt:
     print "Ctrl-C"
-  finally:
-#    ws.close()
-    mc.disconnect()
 
