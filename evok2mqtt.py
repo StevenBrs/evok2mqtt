336d17d2a3fe3205c53bc86687dfea1a924960aa
Steven Broos <steven@Yoga.localdomain>
2022-05-02 15:22:05 +0200

    v2 - mqtt with auto discovery

--- evok2mqtt.py
+++ evok2mqtt.py
@@ -1,110 +1,78 @@
 import json
-import websocket
+import requests
 import paho.mqtt.client as mqtt
 import time
-from subprocess import call
 
-try:
-  import thread
-except ImportError:
-  import _thread as thread
+evokhost = 'http://192.168.0.20:88'
+mqtthost = '192.168.0.21'
 
+devtypes = {'input':'binary_sensor', 'relay':'switch', 'ai':'sensor', 'ao':'number', 'led':'switch'}
+#devtypes = {'input':'binary_sensor'}
+configs = {
+  'input': { 'payload_on':'1','payload_off':'0','initial_state':'0' },
+  'relay': { 'payload_on':'1','payload_off':'0' },
+  'ao': {'min':0, 'max':10,'step':.1},
+  'led': { 'payload_on':'1','payload_off':'0' },
+}
 
-def ws_on_message(ws, message):
-  payloads = json.loads(message)
-  for payload in payloads:
-    if payload['dev'] == 'wd':
-      wds[payload['circuit']] = time.time()
-    elif payload['dev'] not in ['wd', 'ai']:
-      print "WS IN  : ", payload
-      topic = "neuron/" + payload["dev"] + "/" + payload["circuit"]
-      print "MC OUT : ", topic, json.dumps(payload)
-      mc.publish(topic, payload=json.dumps(payload), retain=False)
-
-def mc_on_message(mc, userdata, message):
-  print "MC IN  : ", message.topic, message.payload 
-  topic = message.topic.split('/')
-  try:
-    cmd = json.loads(message.payload)["cmd"]
-  except:
-    cmd = "set"
-  try:
-    payload = json.loads(message.payload)["value"]
-  except:
-    payload = message.payload
-  if len(topic) == 1:
-    wsmsg = {"cmd":cmd}
-  elif len(topic) == 3:
-    wsmsg = {"cmd":cmd,
-        "dev":topic[1],
-        "circuit":topic[2],
-        "value": payload
-      }
-  print "WS OUT : ", json.dumps(wsmsg)
-  ws.send(json.dumps(wsmsg))
-
-def ws_on_error(ws, error):
-  print(error)
-
-def ws_on_close(ws):
-  print("### websocket to evok closed ###")
-
-def mc_on_connect(mc, userdata, flags, rc):
-  #print("Connected with result code "+str(rc))
-  mc.subscribe("neuron/#")
-  
-
-def ws_on_open(ws):
-  def run(*args):
-    for i in range(3):
-      time.sleep(1)
-      print("Hello %d" % i)
-      ws.send("Hello %d" % i)
-    time.sleep(1)
-    ws.close()
-    print("thread terminating...")
-  thread.start_new_thread(run, ())
-
-def watchdog():
-  while True:
-    ok = True
-    for wd in wds:
-      if time.time() - wds[wd] > 5:
-        ok = False
-        print "Neurontcp reset : ", wd, " (", wds[wd], ")"
-        call (["/bin/systemctl", "restart", "neurontcp"])
-        time.sleep(5)
-
-    if ok:
-      text = ""
-      for wd in wds:
-        text += " - " + wd + " : "
-        text += "%.2f " % (time.time() - wds[wd])
-#      print "Watchdogs OK", text
-
-    time.sleep(5)
-
-
-if __name__ == "__main__":
-  wds = {}
-
-  #websocket.enableTrace(False)
-  ws = websocket.WebSocketApp("ws://127.0.0.1/ws",
-                on_message = ws_on_message,
-                on_error = ws_on_error,
-                #ws.on_open = ws_on_open
-                )
+def publish_dev_config():
+  rest_all = requests.get(evokhost + '/rest/all')
+  rest_all_devs = json.loads(rest_all.content.decode('utf-8'))
 
   mc = mqtt.Client(client_id="evok2mqtt", clean_session=False)
-  mc.on_connect = mc_on_connect
-  mc.on_message = mc_on_message
-  mc.connect("127.0.0.1", 1883, 60)
+  mc.connect(mqtthost)
 
-  try:
-    thread.start_new_thread(watchdog,())
-    mc.loop_start()
-    #thread.start_new_thread(ws.run_forever,())
-    ws.run_forever()
-  except KeyboardInterrupt:
-    print "Ctrl-C"
+  for dev in rest_all_devs:
+    #if (ent['dev'] in ['input', 'relay', 'ai', 'ao', 'led']):
+    if (dev['dev'] in devtypes):
+      entity_id='evok2_' + dev['dev'] + '_' + dev['circuit']
+      topic='homeassistant/' + devtypes[dev['dev']] + '/' + entity_id
+      payload = {
+        'unique_id':entity_id,
+        'name':entity_id,
+        'command_topic':topic + '/set',
+        'state_topic':topic,
+      }
+      if (dev['dev'] in configs):
+        print(payload)
+        print(configs[dev['dev']])
+        #payload = payload | configs[dev['dev']]
+        payload = dict(list(payload.items()) + list(configs[dev['dev']].items()))
 
+      print(payload)
+      mc.publish(topic + '/config', payload=json.dumps(payload),retain=True)
+      mc.publish(topic, payload=dev['value'])
+      time.sleep(.01)
+
+  mc.disconnect
+
+
+publish_dev_config()
+
+
+
+# Configuration topic: homeassistant/switch/irrigation/config
+# State topic: homeassistant/switch/irrigation/state
+# Command topic: homeassistant/switch/irrigation/set
+# Payload: {"name": "garden", "command_topic": "homeassistant/switch/irrigation/set", "state_topic": "homeassistant/switch/irrigation/state"}
+
+
+#    payload = {
+#      'unique_id':entity_id,
+#      'valuetemplate':'{{value_json.value}}',
+#      'state_topic':'homeassistant/sensor/' + entity_id + '/state'
+#
+#        'webdata_now_p':{'name':'Solis Current Power (W)', 'device_class':'power', 'state_class':'measurement', 'unit_of_measurement':'W', 'icon':'mdi:solar-power'},
+#        'webdata_today_e':{'name':'Solis Yield Today (kWh)', 'device_class':'energy', 'state_class':'total_increasing', 'unit_of_measurement':'kWh', 'icon':'mdi:solar-power'},
+#        'webdata_total_e':{'name':'Solis Total Yield (kWh)', 'device_class':'energy', 'state_class':'total_increasing', 'unit_of_measurement':'kWh', 'icon':'mdi:solar-power'},
+#        'webdata_alarm':{'name':'Solis Alarms', 'icon':'mdi:solar-power'},
+#
+#  - platform: mqtt
+#    name: EVOK_RELAY_1_04
+#    state_topic: neuron/relay/1_04
+#    payload_on: 1
+#    command_topic: neuron/relay/1_04
+#    payload_off: 0
+#    value_template: '{{value_json.value}}'
+#
+#}
