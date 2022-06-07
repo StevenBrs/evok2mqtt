8be7d8e5eeeeddcdeb0abc23a06e0740db622155
Steven Broos <steven@floor5.be>
2022-06-07 23:05:37 +0200

    Working, to clean

--- evok2mqtt.py
+++ evok2mqtt.py
@@ -2,13 +2,14 @@ import json
 import requests
 import paho.mqtt.client as mqtt
 import time
+import websocket
 
 
 evokhost = 'http://192.168.0.20:88'
 mqtthost = '192.168.0.21'
 
-devtypes = {'input':'binary_sensor', 'relay':'switch', 'ai':'sensor', 'ao':'number', 'led':'switch'}
-#devtypes = {'input':'binary_sensor'}
+#devtypes = {'input':'binary_sensor', 'relay':'switch', 'ai':'sensor', 'ao':'number', 'led':'switch'}
+devtypes = {'input':'binary_sensor', 'relay':'switch', 'led':'switch'}
 configs = {
   'input': { 'payload_on':'1','payload_off':'0','initial_state':'0' },
   'relay': { 'payload_on':'1','payload_off':'0' },
@@ -16,64 +17,78 @@ configs = {
   'led': { 'payload_on':'1','payload_off':'0' },
 }
 
+def get_entity(dev, circuit):
+  return 'evok2_' + dev + '_' + circuit
+
+def get_topic(dev, circuit, sub=''):
+  return 'homeassistant/' + devtypes[dev] + '/neuron-m203/' + circuit + ('/' + sub if sub else '')
+
 def publish_dev_config():
   rest_all = requests.get(evokhost + '/rest/all')
   rest_all_devs = json.loads(rest_all.content.decode('utf-8'))
 
-  mc = mqtt.Client(client_id="evok2", clean_session=False)
-  mc.connect(mqtthost)
-  mc.loop_start()
-
   for dev in rest_all_devs:
     if (dev['dev'] in devtypes):
-      entity_id='evok2_' + dev['dev'] + '_' + dev['circuit']
-      topic='homeassistant/' + devtypes[dev['dev']] + '/neuron-m203/' + entity_id
+      config_topic = get_topic(dev['dev'], dev['circuit'], 'config')
+      state_topic = get_topic(dev['dev'], dev['circuit'], 'state')
+      command_topic = get_topic(dev['dev'], dev['circuit'], 'set')
+      ha_id = 'evok_' + dev['dev'] + '_' + dev['circuit']
+      ha_name = 'Evok2 ' + dev['dev'].capitalize() + ' ' + dev['circuit']
 
       payload = {
-        'unique_id':entity_id,
-        'name':entity_id,
-        'state_topic':topic + '/state',
-        'command_topic':topic + '/set',
+        'unique_id': ha_id,
+        'name': ha_name,
+        'state_topic':state_topic,
+        'command_topic':command_topic
       }
       if (dev['dev'] in configs):
         payload = dict(list(payload.items()) + list(configs[dev['dev']].items()))
 
-      mc_result = mc.publish(topic + '/config', payload=json.dumps(payload), qos=1, retain=True)
-      mc.publish(topic + '/state', payload=dev['value'], retain=False)
+      mc.publish(config_topic, payload=json.dumps(payload), qos=1, retain=True)
+      mc.publish(state_topic, payload=dev['value'], qos=1)
 
-  mc.loop_stop()
-  mc.disconnect
+def mc_connect(mc, userdata, flags, rc):
+  for devtype in devtypes:
+    print('subscribe homeassistant/' + devtype + '/neuron-m203/+/set')
+    mc.subscribe('homeassistant/' + devtype + '/neuron-m203/+/set')
+
+def ws_msg(ws, msg):
+    objs = json.loads(msg)
+    for obj in objs:
+      if obj['dev'] in devtypes:
+        topic = get_topic(obj['dev'], obj['circuit'], 'state')
+        mc.publish(topic, payload=obj['value'], qos=1)
+
+def mc_msg(mc, userdata, message):
+  topics = message.topic.split('/')
+  print(topics)
+  if len(topics) >= 4:
+    if topics[2] == 'neuron-m203' and topics[4] == 'set':
+      dev = list(devtypes.keys())[list(devtypes.values()).index(topics[1])]
+      print(message.payload)
+      payload = {
+            'cmd': 'set',
+            'dev':dev,
+            'circuit':topics[3],
+            'value':int(message.payload),
+            }
+      ws.send(json.dumps(payload))
 
 
 
+
+mc = mqtt.Client(client_id="evok2", clean_session=False)
+mc.on_connect = mc_connect
+mc.on_message = mc_msg
+mc.connect(mqtthost)
+mc.loop_start()
 
 publish_dev_config()
 
+ws = websocket.WebSocketApp('ws://192.168.0.20:8080/ws',
+        on_message = ws_msg
+        )
+ws.run_forever()
 
-
-
-# Configuration topic: homeassistant/switch/irrigation/config
-# State topic: homeassistant/switch/irrigation/state
-# Command topic: homeassistant/switch/irrigation/set
-# Payload: {"name": "garden", "command_topic": "homeassistant/switch/irrigation/set", "state_topic": "homeassistant/switch/irrigation/state"}
-
-
-#    payload = {
-#      'unique_id':entity_id,
-#      'valuetemplate':'{{value_json.value}}',
-#      'state_topic':'homeassistant/sensor/' + entity_id + '/state'
-#
-#        'webdata_now_p':{'name':'Solis Current Power (W)', 'device_class':'power', 'state_class':'measurement', 'unit_of_measurement':'W', 'icon':'mdi:solar-power'},
-#        'webdata_today_e':{'name':'Solis Yield Today (kWh)', 'device_class':'energy', 'state_class':'total_increasing', 'unit_of_measurement':'kWh', 'icon':'mdi:solar-power'},
-#        'webdata_total_e':{'name':'Solis Total Yield (kWh)', 'device_class':'energy', 'state_class':'total_increasing', 'unit_of_measurement':'kWh', 'icon':'mdi:solar-power'},
-#        'webdata_alarm':{'name':'Solis Alarms', 'icon':'mdi:solar-power'},
-#
-#  - platform: mqtt
-#    name: EVOK_RELAY_1_04
-#    state_topic: neuron/relay/1_04
-#    payload_on: 1
-#    command_topic: neuron/relay/1_04
-#    payload_off: 0
-#    value_template: '{{value_json.value}}'
-#
-#}
+mc.loop_stop()
+mc.disconnect
